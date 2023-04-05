d225828bf6b7c0d7450c5bd158100802ce28ef40
Joshua Lee <josh.lee134@hotmail.com>
2023-04-04 23:08:01 -0700

    add styling

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -16,21 +16,20 @@ const canvasCtx = canvas.getContext('2d');
 
 //audio set up
 const audioElement = document.getElementById('audio-element');
-let audioSource;
 let analyzer;
+let audioSource;
 
 
 // visualizer event listener for change
 let visualizerShapeSelector = document.getElementById('shape-selector')
 
 
-
 //visualizer function
 export function visualizer(drawerFunc){
   const audioContext = new AudioContext();
   audioSource = audioContext.createMediaElementSource(audioElement);
-  analyzer = audioContext.createAnalyser();
   audioSource.connect(audioContext.destination);
+  analyzer = audioContext.createAnalyser();
   audioSource.connect(analyzer);
   analyzer.fftSize = fftSizeValue;
   const bufferLength = analyzer.frequencyBinCount;
@@ -49,11 +48,37 @@ export function visualizer(drawerFunc){
   }
 
   animation();
-  visualizerShapeSelector.addEventListener('input', function(){
-    drawSelector();
+  
+  visualizerShapeSelector.addEventListener('change', function(){
+    reVisualizer();
   });
 }  
 
+function reVisualizer(drawerFunc = visualizerShapeSelector.value === 'rectangle' ? rectangularDrawer : circularDrawer){
+  if(audioSource !== null) {
+    audioSource.disconnect();
+  }
+  audioSource.connect(audioContext.destination)
+  analyzer = audioContext.createAnalyser();
+  audioSource.connect(analyzer);
+  analyzer.fftSize = fftSizeValue;
+  const bufferLength = analyzer.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = canvas.width/(Math.floor(bufferLength - 5))
+  let barHeight;
+  let xPos;
+  
+  function animation(){
+      xPos = 0;
+      canvasCtx.clearRect(0,0,canvas.width, canvas.height)
+      analyzer.getByteFrequencyData(dataArray);
+      drawerFunc(bufferLength, xPos, barWidth, barHeight, dataArray);
+      requestAnimationFrame(animation);
+  }
+
+  animation();
+}
 
 function updateVisualizer() {
   if (analyzer && fftSizeValue) {
@@ -68,6 +93,7 @@ fftSizeInput.addEventListener('input', function(){
   updateVisualizer();
 });
 
+
 export function drawSelector(){
   if (visualizerShapeSelector.value === 'rectangle') {
     visualizer(rectangularDrawer);
@@ -75,4 +101,6 @@ export function drawSelector(){
     visualizer(circularDrawer);
   }
 }
+
+
 
